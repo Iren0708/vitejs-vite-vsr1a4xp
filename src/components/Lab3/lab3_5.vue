ced5e866a431df959821602cc86ba4d3efa2fc99
Iren0708 <sti4070802@mail.ru>
2025-03-06 18:15:09 +0300

    main

--- src/components/Lab3/lab3_5.vue
+++ src/components/Lab3/lab3_5.vue
@@ -1,12 +1,58 @@
 <template>
-
-  <v-dialog open="open"></v-dialog>
   
+      <v-dialog max-width="500">
+        <template v-slot:activator="{ props: activatorProps }">
+          <v-btn
+            v-bind="activatorProps"
+            color="surface-variant"
+            text="Открыть диалог"
+            variant="flat"
+          ></v-btn>
+        </template>
+
+        <template v-slot:default="{ isActive }">
+          <v-card title="Диалог">
+            <v-card-text>
+              
+              <v-text-field 
+              v-model="mes">
+              </v-text-field>
+              <v-text-field 
+              v-model="mes2">
+              </v-text-field>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              
+              <v-btn @click="vyvod" text="вывод в консоль" />
+              <v-btn
+                text="Закрыть диалог"
+                @click="isActive.value = false"
+              ></v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-dialog>
+    
 </template>
 
-<script>
+
+<script >
 export default {
   name: "lab3_5",
+  data(){
+    return {
+      mes:'123',
+      mes2:'ipjl',
+
+    }
+  },
+  methods:{
+    vyvod(){
+      console.log(this.mes,this.mes2)
+    }
   }
 
+};
 </script>
